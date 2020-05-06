33dee7e0c909f9f7508bf114a204e61d420b59d6
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-06 18:22:36 +0100

    Added comments

--- BuilderDesign/ProductStockReportDirector.cs
+++ BuilderDesign/ProductStockReportDirector.cs
@@ -4,15 +4,24 @@ using System.Text;
 
 namespace BuilderDesign
 {
+    /*
+     * Class to represent builder object
+     */
     public class ProductStockReportDirector
     {
         private readonly IProductStockReportBuilder _productStockReportBuilder;
 
+        /*
+         * Takes the builder logic as parameter
+         */
         public ProductStockReportDirector(IProductStockReportBuilder productStockReportBuilder)
         {
             _productStockReportBuilder = productStockReportBuilder;
         }
 
+        /*
+         * Builds the report following the model Header - Body - Footer
+         */
         public void BuildStockReport()
         {
             _productStockReportBuilder
--- BuilderDesign/Program.cs
+++ BuilderDesign/Program.cs
@@ -10,6 +10,9 @@ namespace BuilderDesign
     {
         static void Main(string[] args)
         {
+            /*
+             * Create list of products and populate it
+             */
             var products = new List<Product>
             {
                 new Product { Name = "Monitor", Price = 199.98 },
@@ -17,10 +20,17 @@ namespace BuilderDesign
                 new Product { Name = "Mouse", Price = 10.99}
             };
 
+            /*
+             * Create builder logic with product list as argument and 
+             * director with builder logic as argument then build the report
+             */
             var builder = new ProductStockReportBuilder(products);
             var director = new ProductStockReportDirector(builder);
             director.BuildStockReport();
 
+            /*
+             * Print the report
+             */
             var report = builder.GetReport();
             Console.WriteLine(report);
 
