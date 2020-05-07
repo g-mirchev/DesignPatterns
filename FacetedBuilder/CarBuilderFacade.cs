cf3ac239906b6e1f74d0304a05ab7233e9fe3541
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 15:21:00 +0100

    Added facade for the car builder

---
+++ FacetedBuilder/CarBuilderFacade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacetedBuilder
+{
+    public class CarBuilderFacade
+    {
+        protected Car Car { get; set; }
+
+        public CarBuilderFacade()
+        {
+            Car = new Car();
+        }
+
+        public Car Build() => Car;
+    }
+}
