1a22047e66aa681dfb51ac227326872cb758c157
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 11:21:25 +0100

    Added salary builder

---
+++ FluentBuilderRecursiveGenerics/EmployeeSalaryBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentBuilderRecursiveGenerics
+{
+    /*
+     * Creating salary builder for employee object inherited from position builder implementing
+     * generic recursive inheritence
+     */
+    class EmployeeSalaryBuilder<T> : EmployeePositionBuilder<EmployeeSalaryBuilder<T>> where T : EmployeeSalaryBuilder<T>
+    {
+        public T WithSalary(double salary)
+        {
+            employee.Salary = salary;
+            return (T)this;
+        }
+    }
+}
