49875d9a48218a072bd078468fa482e20bb455e7
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 15:44:30 +0100

    Added builders for the different parts of the Car model

---
+++ FacetedBuilder/CarAddressBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacetedBuilder
+{
+    /*
+     * Logic for building car manufacturer info
+     */
+    class CarAddressBuilder : CarBuilderFacade
+    {
+        /*
+         * Creates the manufacturing section of the car taking a car as parameter
+         * and instantiating it through inheritance
+         */
+        public CarAddressBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        /*
+         * Build the car city
+         */
+        public CarAddressBuilder InCity(string city)
+        {
+            Car.City = city;
+            return this;
+        }
+
+        /*
+         * Build the car address
+         */
+        public CarAddressBuilder AtAddress(string address)
+        {
+            Car.Address = address;
+            return this;
+        }
+    }
+}
---
+++ FacetedBuilder/CarInfoBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacetedBuilder
+{
+    /*
+     * Logic for building car describtion info
+     */
+    public class CarInfoBuilder : CarBuilderFacade
+    {
+        /*
+         * Create car info section taking car as parameter
+         * and instantiating it through inheritance
+         */
+        public CarInfoBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        /*
+         * Build the car type
+         */
+        public CarInfoBuilder WithType(string type)
+        {
+            Car.Type = type;
+            return this;
+        }
+
+        /*
+         * Build the car color
+         */
+        public CarInfoBuilder WithColor(string color)
+        {
+            Car.Color = color;
+            return this;
+        }
+
+        /*
+         * Build the number of doors for the car
+         */
+        public CarInfoBuilder WithNumberOfDoors(int number)
+        {
+            Car.NumberOfDoors = number;
+            return this;
+        }
+    }
+}
