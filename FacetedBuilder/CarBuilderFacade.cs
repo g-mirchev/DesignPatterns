e8393ba7839bd8f5e80c5f556fb027bf4da524f8
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 15:24:46 +0100

    Added comments

--- FacetedBuilder/CarBuilderFacade.cs
+++ FacetedBuilder/CarBuilderFacade.cs
@@ -4,15 +4,25 @@ using System.Text;
 
 namespace FacetedBuilder
 {
+    /*
+     * Facade for the car builder instantiates the car object
+     * to be inherited by other builders
+     */
     public class CarBuilderFacade
     {
         protected Car Car { get; set; }
 
+        /*
+         * Instantiate the car object
+         */
         public CarBuilderFacade()
         {
             Car = new Car();
         }
 
+        /*
+         * Return the car object
+         */
         public Car Build() => Car;
     }
 }
