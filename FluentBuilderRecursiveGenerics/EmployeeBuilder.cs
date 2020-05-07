23dab3f8ec571b1d18f07202b44492d9b95d31d6
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 11:07:30 +0100

    Added abstract Employee Builder class for inheritence

---
+++ FluentBuilderRecursiveGenerics/EmployeeBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentBuilderRecursiveGenerics
+{
+    /*
+     * Absrtract class for builder elements
+     */
+    public abstract class EmployeeBuilder
+    {
+        protected Employee employee;
+
+        /*
+         * Create new employee
+         */
+        public EmployeeBuilder()
+        {
+            employee = new Employee();
+        }
+
+        /*
+         * Return employee
+         */
+        public Employee Build() => employee;
+    }
+}
