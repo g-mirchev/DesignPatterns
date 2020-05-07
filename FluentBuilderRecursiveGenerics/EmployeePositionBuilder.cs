117059be4ba53977e0e34e5ea71bccd401c8af6f
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 11:01:29 +0100

    Added class to build Position part of Employee

---
+++ FluentBuilderRecursiveGenerics/EmployeePositionBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentBuilderRecursiveGenerics
+{
+    /*
+     * Class to build position section of Eployee
+     */
+    public class EmployeePositionBuilder : EmployeeInfoBuilder
+    {
+        public EmployeePositionBuilder AtPosition(string position)
+        {
+            employee.Position = position;
+            return this;
+        }
+    }
+}
