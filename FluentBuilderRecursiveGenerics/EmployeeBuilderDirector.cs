a7b3e78118ab92a2c67377ec651a16bd15e5a086
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 11:27:43 +0100

    Added an API to enable fluent interface inheritance with recursive gnerics

---
+++ FluentBuilderRecursiveGenerics/EmployeeBuilderDirector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentBuilderRecursiveGenerics
+{
+    /*
+     * Represents API to build Employee object inherits from EmployeeSalaryBuilder and sets the
+     * type for the recursive generic approach enabling fluent interface inheritance
+     */
+    class EmployeeBuilderDirector : EmployeeSalaryBuilder<EmployeeBuilderDirector>
+    {
+        public static EmployeeBuilderDirector NewEmployee => new EmployeeBuilderDirector();
+    }
+}
