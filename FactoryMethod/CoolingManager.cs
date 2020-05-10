34d8e0ad0910ec80c2db5467b41b35d76287f959
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-10 15:27:36 +0100

    Created warming and cooling managers for the AC

---
+++ FactoryMethod/CoolingManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Model for handling cooling on the AC
+     */
+    public class CoolingManager : IAirConditioner
+    {
+        private readonly double _temperature;
+
+        /*
+         * Construct manager and take temperature as param
+         */
+        public CoolingManager(double temperature)
+        {
+            _temperature = temperature;
+        }
+
+        /*
+         * Implementing the interface method for cooling
+         */
+        public void Operate()
+        {
+            Console.WriteLine($"Cooling the room to the required temperature of {_temperature} degrees");
+        }
+    }
+}
---
+++ FactoryMethod/WarmingManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Model for handling warming on AC
+     */
+    public class WarmingManager : IAirConditioner
+    {
+        private readonly double _temperature;
+
+        /*
+        * Construct manager and take temperature as param
+        */
+        public WarmingManager(double temperature)
+        {
+            _temperature = temperature;
+        }
+
+        /*
+        * Implementing the interface method for warming
+        */
+        public void Operate()
+        {
+            Console.WriteLine($"Warming the room to the required temperature of {_temperature} degrees");
+        }
+    }
+}
