406a7d2c50e1cac0b40c523a52999bdba0982ce1
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-10 15:32:10 +0100

    Added abstract model for Cooling and Warming factories

---
+++ FactoryMethod/AirConditionerFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Abstract model for AC manager factories
+     */
+    public abstract class AirConditionerFactory
+    {
+        public abstract IAirConditioner Create(double temperature);
+    }
+}
