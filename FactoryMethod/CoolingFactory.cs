e48020a7310341170aed8dc8ba305dac94b5b9db
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-10 15:37:27 +0100

    Added warming and cooling factory classes

---
+++ FactoryMethod/CoolingFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Model for cooling manager factory
+     */
+    public class CoolingFactory : AirConditionerFactory
+    {
+        /*
+         * Create and return new coling manager with the param temperature as arg
+         */
+        public override IAirConditioner Create(double temperature) => new CoolingManager(temperature);
+    }
+}
---
+++ FactoryMethod/WarmingFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Model for warming manager factory
+     */
+    public class WarmingFactory : AirConditionerFactory
+    {
+        public override IAirConditioner Create(double temperature) => new WarmingManager(temperature);
+    }
+}
