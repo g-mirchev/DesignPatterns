1403d74cdd22e415c983e91096aecfd212d9f52f
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-10 16:23:27 +0100

    Added class to represent AC and all it's fucntionality

---
+++ FactoryMethod/AirConditioner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Model to represent AC and map Action enum to appropriate factory
+     */
+    public class AirConditioner
+    {
+        private readonly Dictionary<Actions, AirConditionerFactory> _factories;
+
+        /*
+         * instantiate dictionary an dynamically populate mapping each Action from the enumerables
+         * to a newly created instance of the corresponding factory type
+         */
+        public AirConditioner()
+        {
+            _factories = new Dictionary<Actions, AirConditionerFactory>();
+
+            foreach (Actions action in Enum.GetValues(typeof(Actions)))
+            {
+                var factory = (AirConditionerFactory)Activator.CreateInstance(Type.GetType("FactoryMethod." + Enum.GetName(typeof(Actions), action) + "Factory"));
+                _factories.Add(action, factory);
+            }
+        }
+
+        /*
+         * Execute appropriate factory creation
+         */
+        public IAirConditioner ExecuteCreation(Actions action, double temperature) => _factories[action].Create(temperature);
+    }
+}
