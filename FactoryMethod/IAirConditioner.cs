420529e19ecfd35023f510455ef4856c98c50843
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-10 15:17:59 +0100

    Created interface for air conditioner operators

---
+++ FactoryMethod/IAirConditioner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Interface for air contitioner controllers
+     */
+    public interface IAirConditioner
+    {
+        void Operate();
+    }
+}
