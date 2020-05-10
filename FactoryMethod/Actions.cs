064e631b04f2db4f1d9ea5ed64adaacd14f022c8
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-10 15:56:45 +0100

    added enum for AC operations

---
+++ FactoryMethod/Actions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /*
+     * Enumerable to represent AC operations
+     */
+    public enum Actions
+    {
+        Cooling,
+        Warming
+    }
+}
