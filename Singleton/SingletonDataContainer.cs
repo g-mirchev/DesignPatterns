ccd8c15932015dafcc67647586c547187eaabc63
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-11 16:06:54 +0100

    Added data container for capitals and their populations

---
+++ Singleton/SingletonDataContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Singleton
+{
+    /*
+     * Represents the data container for capitals and their populations as a dictionary
+     */
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> _capitals = new Dictionary<string, int>();
+
+        /*
+         * Read all the lines from the txt and populate the Dictionary iterating trough two lines at a time
+         * the first line of each line pair being the capital, the second its population
+         */
+        public SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("capitals.txt");
+            for (int i = 0; i < elements.Length; i+=2)
+            {
+                _capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+
+        /*
+         * Implementing the interface, return the population mapped to the parsed capital
+         */
+        public int GetPopulation(string name)
+        {
+            return _capitals[name];
+        }
+    }
+}
