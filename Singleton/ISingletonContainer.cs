e0f04fbe8fe7071aa3b1a9167b145860307f9fc1
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-11 15:52:14 +0100

    Added singleton to the container

---
+++ Singleton/ISingletonContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    /*
+     * Interface for the singleton container
+     */
+    public interface ISingletonContainer
+    {
+        int GetPopulation(string name);
+    }
+}
