55f3abc77479bbbc4a0799ca15fa76bcb3386f4b
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-11 16:33:12 +0100

    Added example in main

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -2,10 +2,18 @@
 
 namespace Singleton
 {
+    /*
+     * Demonstrates the use of singleton design pattern which is a rarely used
+     * design pattern in which an object has to be created only once and made thread safe
+     */
     class Program
     {
         static void Main(string[] args)
         {
+            var db = SingletonDataContainer.Instance;
+            Console.WriteLine(db.GetPopulation("Washington, D.C."));
+            var db2 = SingletonDataContainer.Instance;
+            Console.WriteLine(db2.GetPopulation("London"));
         }
     }
 }
